57e29d2ac1649d8ff56bc7eee50963dc8df8ddda
samuel <samuel.reyes.sanz@gmail.com>
2024-01-26 13:46:25 +0100

    model and train properly commented and pep8 standards

--- project/model/train.py
+++ project/model/train.py
@@ -7,18 +7,23 @@ from torch.utils.data import Dataset, DataLoader
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-num_epochs = 2
 
 class CustomDataset(Dataset):
     """
-        Initializes the dataset with the data and sequence length.
+    Custom dataset for handling paired upper and surface data along with labels.
 
-        :param upper_data: 'Upper' data (T, L, W, H, C).
-        :param surface_data: 'surface' data (T, W, H, C).
-        :param labels: Labels (T, L, W, H, C).
-        :param sequence_length: Length of the input sequence.
-        :param delay: Delay for the label in time.
+    This dataset is designed to work with sequential data, where each sequence 
+    has a specified length and is associated with a delayed label. It's suitable 
+    for time-series predictions where the goal is to predict future values 
+    based on past sequences of data.
+
+    :param upper_data: Numpy array representing 'Upper' data of shape (T, L, W, H, C).
+    :param surface_data: Numpy array representing 'surface' data of shape (T, W, H, C).
+    :param labels: Numpy array representing labels of shape (T, L, W, H, C).
+    :param sequence_length: Integer representing the length of the input sequence.
+    :param delay: Integer representing the delay for the label in time steps.
     """
+
     def __init__(self, upper_data, surface_data, labels, sequence_length, delay):
         self.upper_data = upper_data
         self.surface_data = surface_data
@@ -27,16 +32,20 @@ class CustomDataset(Dataset):
         self.delay = delay
 
     def __len__(self):
+        # Calculate the length of the dataset accounting for sequence length and delay
         return len(self.upper_data) - self.sequence_length - self.delay
 
     def __getitem__(self, idx):
+        # Ensure that the requested index is within the bounds of the dataset
         if idx + self.sequence_length + self.delay >= len(self.upper_data):
             raise IndexError("Index out of range")
 
-        upper_sequence = self.upper_data[idx : idx + self.sequence_length]
-        surface_sequence = self.surface_data[idx : idx + self.sequence_length]
+        # Extract sequences for upper and surface data, and the corresponding label
+        upper_sequence = self.upper_data[idx: idx + self.sequence_length]
+        surface_sequence = self.surface_data[idx: idx + self.sequence_length]
         label = self.labels[idx + self.sequence_length + self.delay]
 
+        # Convert numpy arrays to torch tensors and return as a dictionary
         data = {
             'upper': torch.from_numpy(upper_sequence).float(),
             'surface': torch.from_numpy(surface_sequence).float(),
@@ -44,8 +53,19 @@ class CustomDataset(Dataset):
         }
         return data
 
+
 def train_model(config: dict):
-    
+    """
+    Trains the ViViT model based on the provided configuration.
+
+    This function initializes the ViViT model with the given parameters, loads
+    the training and validation datasets, and performs training for a specified
+    number of epochs. It also handles validation and model saving.
+
+    :param config: Dictionary containing the configuration settings.
+    """
+
+    # Extract model and training parameters from the config
     image_size_3d = config['model']['image_size_3d']
     patch_size_3d = config['model']['patch_size_3d']
     image_size_2d = config['model']['image_size_2d']
@@ -66,8 +86,9 @@ def train_model(config: dict):
     epochs = config['train']['epochs']
 
     # Load data
-    train_data_path = os.path.join(config['global']['path'], config['global']['train_data_path'])
-   
+    train_data_path = os.path.join(
+        config['global']['path'], config['global']['train_data_path'])
+
     upper_train = np.load(os.path.join(train_data_path, 'upper_train.npy'))
     surface_train = np.load(os.path.join(train_data_path, 'surface_train.npy'))
     labels_train = np.load(os.path.join(train_data_path, 'labels_train.npy'))
@@ -76,55 +97,58 @@ def train_model(config: dict):
     surface_val = np.load(os.path.join(train_data_path, 'surface_val.npy'))
     labels_val = np.load(os.path.join(train_data_path, 'labels_val.npy'))
 
-    # Create dataset and dataloader
-    train_dataset = CustomDataset(upper_train, surface_train, labels_train, sequence_length=sequence_length, delay=delay)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+    # Create dataset and dataloader for training and validation
+    train_dataset = CustomDataset(
+        upper_train, surface_train, labels_train, sequence_length=sequence_length, delay=delay)
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=False)
 
-    val_dataset = CustomDataset(upper_val, surface_val, labels_val, sequence_length=sequence_length, delay=delay)
+    val_dataset = CustomDataset(
+        upper_val, surface_val, labels_val, sequence_length=sequence_length, delay=delay)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
-    # Create an instance of the model
-    model = ViViT(image_size_3d, patch_size_3d, image_size_2d, patch_size_2d, output_dim, dim, depth, heads, dim_head, dropout, emb_dropout, scale_dim).to(device)
-    
-    # Loss function and optimizer
+    # Initialize the model and move it to the configured device
+    model = ViViT(image_size_3d, patch_size_3d, image_size_2d, patch_size_2d, output_dim,
+                  dim, depth, heads, dim_head, dropout, emb_dropout, scale_dim).to(device)
+
+    # Loss function and optimizer setup
     loss_fn = torch.nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    # Train cycle
+    # Training loop
     for epoch in range(epochs):
-        model.train()
-        # Training
+        model.train()  # Set model to training mode
+        # Training iteration over the dataset
         for data in train_loader:
-            upper, surface, labels = data['upper'].to(device), data['surface'].to(device), data['label'].to(device)
-
-            # Forward pass
+            upper, surface, labels = data['upper'].to(
+                device), data['surface'].to(device), data['label'].to(device)
             outputs = model(upper, surface)
             loss = loss_fn(outputs, labels)
 
-            # Backward pass
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-        # Validation
-        model.eval() 
+        # Validation phase
+        model.eval()
+        val_loss = 0
         with torch.no_grad():
-            val_loss = 0
+            # Iterate over validation data
             for data in val_loader:
-                upper, surface, labels = data['upper'].to(device), data['surface'].to(device), data['label'].to(device)
-
-                # Forward pass
+                upper, surface, labels = data['upper'].to(
+                    device), data['surface'].to(device), data['label'].to(device)
                 outputs = model(upper, surface)
                 val_loss += loss_fn(outputs, labels).item()
-
             val_loss /= len(val_loader)
 
-        # Save intermediate model
+        # Save the model periodically and print training progress
         if config['train']['save_model']:
-            torch.save(model.state_dict(), os.path.join(config['global']['path'], config['global']['checkpoints_path'], config['train']['model_name'] + f'_epoch_{epoch+1}.pth'))
+            torch.save(model.state_dict(), os.path.join(
+                config['global']['path'], config['global']['checkpoints_path'], config['train']['model_name'] + f'_epoch_{epoch+1}.pth'))
+        print(
+            f"Epoch [{epoch+1}/{epochs}], Loss: {loss.item()}, Val Loss: {val_loss}")
 
-        print(f"Epoch [{epoch+1}/{epochs}], Loss: {loss.item()}, Val Loss: {val_loss}")
-
-    # Save the model checkpoint
+    # Save the final model
     if config['train']['save_model']:
-        torch.save(model.state_dict(), os.path.join(config['global']['path'], config['global']['checkpoints_path'], config['train']['model_name'] + '.pth'))
+        torch.save(model.state_dict(), os.path.join(
+            config['global']['path'], config['global']['checkpoints_path'], config['train']['model_name'] + '.pth'))
