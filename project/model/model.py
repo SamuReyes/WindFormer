automatic mixed precision, reconstruction head modified, corrected label outputs

project/model/model.py
@@ -234,7 +234,7 @@ class ViViT(nn.Module):
     and aggregation.
     """
 
-    def __init__(self, image_size_3d, patch_size_3d, image_size_2d, patch_size_2d, seq_len, output_dim, dim=192, depth=4, heads=3, dim_head=64, dropout=0., emb_dropout=0., scale_dim=4):
+    def __init__(self, image_size_3d, patch_size_3d, image_size_2d, patch_size_2d, seq_len, output_dim, dim=192, depth=4, heads=3, dim_head=64, dropout=0., reconstr_dropout=0., scale_dim=4):
         super().__init__()
 
         # Patch embedding layers for 3D and 2D data
@@ -253,28 +253,15 @@ class ViViT(nn.Module):
         self.temporal_transformer = Transformer(
             dim, depth, heads, dim_head, dim*scale_dim, dropout)
 
-        # Dropout layer for regularization
-        self.dropout = nn.Dropout(emb_dropout)
-
-        # Linear layer for output dimension adjustment
-        self.output_dim = output_dim
-        self.linear = nn.Linear(dim, reduce(mul, self.output_dim))
-
-        # Aggregation layer
-        # Adaptive average pooling for aggregation
-        self.aggregation = nn.AdaptiveAvgPool1d(1)
-
         # Reconstruction head
-        """
         intermediate_dim = dim * 2
+        self.output_dim = output_dim
         self.reconstruction_head = nn.Sequential(
             nn.Linear(dim, intermediate_dim),
             nn.ReLU(),
-            nn.Dropout(reconstruction_dropout),  # Capa de Dropout
-            # Segunda capa lineal ajustando a la dimensión de salida deseada
+            nn.Dropout(reconstr_dropout),
             nn.Linear(intermediate_dim, reduce(mul, output_dim))
         )
-        """
 
     def create_temporal_attention_mask(self, t, n, device):
         """
@@ -317,9 +304,10 @@ class ViViT(nn.Module):
         x = self.temporal_transformer(x, mask)  # [B, T * N, D]
 
         # Reconstruction to output dimensions
-        x = self.linear(x)  # [B, T * N, output_dim]
-        x = x.transpose(1, 2)  # [B, output_dim, T * N]
-        x = self.aggregation(x).squeeze(2)  # [B, prod(output_dim)]
-        x = x.view(-1, *self.output_dim)  # [B, output_dim]
+        x = rearrange(x, 'b (t n) d -> b (t n) d', t=t)  # [B, T * N, D]
+        # [B, T * N, output_dim] # TODO implement patch recovery
+        x = self.reconstruction_head(x)
+        x = x.view(-1, t, n, self.output_dim)  # [B, T, N, output_dim]
 
+        # TODO try classifying instead of regression
         return x

project/model/train.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import torch
 import torch.nn.functional as F
+from torch.cuda.amp import GradScaler
 from model.model import ViViT
 from torch.utils.data import Dataset, DataLoader
 
@@ -43,7 +44,8 @@ class CustomDataset(Dataset):
         # Extract sequences for upper and surface data, and the corresponding label
         upper_sequence = self.upper_data[idx: idx + self.sequence_length]
         surface_sequence = self.surface_data[idx: idx + self.sequence_length]
-        label = self.labels[idx + self.sequence_length + self.delay]
+        label = self.labels[idx + self.delay: idx +
+                            self.sequence_length + self.delay]
 
         # Convert numpy arrays to torch tensors and return as a dictionary
         data = {
@@ -76,7 +78,7 @@ def train_model(config: dict):
     heads = config['model']['heads']
     dim_head = config['model']['dim_head']
     dropout = config['model']['dropout']
-    emb_dropout = config['model']['emb_dropout']
+    reconstr_dropout = config['model']['reconstr_dropout']
     scale_dim = config['model']['scale_dim']
     delay = config['model']['delay']
 
@@ -109,7 +111,10 @@ def train_model(config: dict):
 
     # Initialize the model and move it to the configured device
     model = ViViT(image_size_3d, patch_size_3d, image_size_2d, patch_size_2d, sequence_length, output_dim,
-                  dim, depth, heads, dim_head, dropout, emb_dropout, scale_dim).to(device)
+                  dim, depth, heads, dim_head, dropout, reconstr_dropout, scale_dim).to(device)
+
+    # Initialize the gradient scaler for mixed precision training
+    scaler = GradScaler()
 
     # Loss function and optimizer setup
     loss_fn = torch.nn.MSELoss()
@@ -120,14 +125,17 @@ def train_model(config: dict):
         model.train()  # Set model to training mode
         # Training iteration over the dataset
         for data in train_loader:
-            upper, surface, labels = data['upper'].to(
-                device), data['surface'].to(device), data['label'].to(device)
-            outputs = model(upper, surface)
-            loss = loss_fn(outputs, labels)
-
             optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            # Runs the forward pass under autocast
+            with torch.autocast(device_type='cuda', dtype=torch.float16):
+                upper, surface, labels = data['upper'].to(
+                    device), data['surface'].to(device), data['label'].to(device)
+                outputs = model(upper, surface)
+                loss = loss_fn(outputs, labels)
+
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
 
         # Validation phase
         model.eval()
@@ -135,10 +143,12 @@ def train_model(config: dict):
         with torch.no_grad():
             # Iterate over validation data
             for data in val_loader:
-                upper, surface, labels = data['upper'].to(
-                    device), data['surface'].to(device), data['label'].to(device)
-                outputs = model(upper, surface)
-                val_loss += loss_fn(outputs, labels).item()
+                # Runs the forward pass under autocast
+                with torch.autocast(device_type=device, dtype=torch.float16):
+                    upper, surface, labels = data['upper'].to(
+                        device), data['surface'].to(device), data['label'].to(device)
+                    outputs = model(upper, surface)
+                    val_loss += loss_fn(outputs, labels).item()
             val_loss /= len(val_loader)
 
         # Save the model periodically and print training progress