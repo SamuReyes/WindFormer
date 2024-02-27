57524e5c5fdb6728d52dea1c979398013bb36bcc
SamuReyes <100429033@alumnos.uc3m.es>
2024-02-27 11:27:36 +0100

    u100 and v100 outputs added

--- project/processing/labelizer.py
+++ project/processing/labelizer.py
@@ -19,17 +19,21 @@ def get_labels(config: dict):
     upper_var_names = config['preprocessing']['upper_var_names']
     surface_var_names = config['preprocessing']['surface_var_names']
 
-    # Determine the indices of 'u10', 'v10' for surface and 'u', 'v' for upper
+    # Determine the indices of 'u10', 'v10', 'u100', 'v100' for surface and 'u', 'v' for upper
     surface_vars_indices = [surface_var_names.index(var) for var in [
         'u10', 'v10']]
+    middle_vars_indices = [surface_var_names.index(var) for var in [
+        'u100', 'v100']]
     upper_vars_indices = [upper_var_names.index(var) for var in ['u', 'v']]
 
+    middle_data = surface_data[:, :, :, middle_vars_indices]
     surface_data = surface_data[:, :, :, surface_vars_indices]
     upper_data = upper_data[:, :, :, :, upper_vars_indices]
 
     # Expand dimensions of surface data and combine it with upper data to form a unified label dataset
     surface_data = np.expand_dims(surface_data, axis=1)
-    combined_labels = np.concatenate((surface_data, upper_data), axis=1)
+    middle_data = np.expand_dims(middle_data, axis=1)
+    combined_labels = np.concatenate((surface_data, middle_data, upper_data), axis=1)
 
     # Save the combined label dataset to a file
     np.save(os.path.join(config['global']['path'], config['global']
